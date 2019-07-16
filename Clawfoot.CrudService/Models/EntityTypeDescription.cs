33eb0885a930ec4b8219de1a95f22840cd3c4f1f
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-07-16 16:40:00 -0700

    Adds entity type description

---
+++ Clawfoot.CrudService/Models/EntityTypeDescription.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.CrudService.Models
+{
+    public class EntityTypeDescription
+    {
+        public EntityTypeDescription(Type entityType, IEntityType efEntityType)
+        {
+            EntityType = entityType;
+            EfEntityType = efEntityType;
+        }
+
+        /// <summary>
+        /// The Type of the entity
+        /// </summary>
+        public Type EntityType { get; private set; }
+
+        /// <summary>
+        /// The ENtity Framework type for the entity
+        /// </summary>
+        public IEntityType EfEntityType { get; private set; }
+
+        /// <summary>
+        /// Valid if the type is an entity or a DTO, 
+        /// </summary>
+        public bool IsValid { get; private set; }
+    }
+}

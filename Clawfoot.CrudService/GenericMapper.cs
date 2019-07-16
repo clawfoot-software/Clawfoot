b031c508739af8cd99edb70f09a8fa4e5f4a914c
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-07-16 15:55:07 -0700

    Adds GenericMapper and GenericMapperMaker

---
+++ Clawfoot.CrudService/GenericMapper.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Clawfoot.CrudService
+{
+    public class GenericMapper<TDto, TEntity>
+        where TDto : class
+        where TEntity : class
+    {
+
+        private readonly DbContext _context;
+        private readonly PropertyInfo _entityKey;
+        private readonly IMapper _mapper;
+
+        public GenericMapper(DbContext context, Type entity, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+            _entityKey = entity.GetProperties()
+                .Where(x => Attribute.IsDefined(x, typeof(KeyAttribute))).SingleOrDefault();
+
+            _mapper = mapper;
+        }
+
+        public PropertyInfo EntityKey
+        {
+            get => _entityKey;
+        }
+
+        //Get and save key property
+        //Create equals predicate from key property and supplied key
+        //Filter DbSet with that key
+        //Project to Dto
+
+        public DbSet<TEntity> GetDbSet()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public TEntity GetEntityByKey(object key)
+        {
+            return GetDbSet().Find(key);
+        }
+
+        public IQueryable<TEntity> GetEntityQueryableByKey(object key)
+        {
+            return GetDbSet().Where(CreateFilter(key));
+        }
+
+        public IQueryable<TDto> GetDtoQueryableByEntityKey(object key)
+        {
+            return GetEntityQueryableByKey(key).ProjectTo<TDto>(_mapper.ConfigurationProvider);
+        }
+
+        public IQueryable<TEntity> GetEntityQueryable()
+        {
+            return GetDbSet().AsQueryable();
+        }
+
+        public IQueryable<TDto> GetDtoQueryable()
+        {
+            return GetDbSet().ProjectTo<TDto>(_mapper.ConfigurationProvider);
+        }
+
+        public object GetEntityFromDto(TDto dto, object entity)
+        {
+            return _mapper.Map(dto, entity);
+        }
+
+        private Expression<Func<TEntity, bool>> CreateFilter(object key)
+        {
+            return QueryUtilities.GetPredicate<TEntity, object>(ExpressionType.Equal, _entityKey.Name, key);
+        }
+    }
+}
---
+++ Clawfoot.CrudService/GenericMapperMaker.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Clawfoot.CrudService
+{
+    public class GenericMapperMaker
+    {
+        /// <summary>
+        /// The GenericMapper instance
+        /// </summary>
+        public dynamic Accessor { get; }
+
+        /// <summary>
+        /// Creates a new GenericMapper with the provided dtoType and entityType
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="dtoType"></param>
+        /// <param name="entityType"></param>
+        /// <param name="mapper"></param>
+        public GenericMapperMaker(DbContext context, Type dtoType, Type entityType, IMapper mapper)
+        {
+            /* Process:
+             *  - Create Generic mapper type with the provided types
+             *  - Get constructor
+             *  - Get Func to new it up
+             *  - Invoke the Func with the parameters
+             */
+
+            Type genericBase = typeof(GenericMapper<,>);
+            Type genericType = genericBase.MakeGenericType(dtoType, entityType);
+            ConstructorInfo constructor = genericType.GetConstructors().Single();
+
+            Accessor = NewGenericMapper(constructor, entityType).Invoke(context, entityType, mapper);
+        }
+
+        private Func<DbContext, Type, IMapper, dynamic> NewGenericMapper(ConstructorInfo constructor, Type entityType)
+        {
+            /* Process:
+             *   - Setup the parameters for new GenericMapper(DbContext context, Type entity, IMapper mapper)
+             *   - Create an expression out of the constructor and arguments
+             *   - Create the lambda expression out of that (I have no idea what this does anymore)
+             *   - Compile it
+             *   - Return the Func to instantiate the GenericMapper
+             */
+
+            ParameterExpression arg1 = Expression.Parameter(typeof(DbContext), "context");
+            ParameterExpression arg2 = Expression.Parameter(typeof(Type), "entity");
+            ParameterExpression arg3 = Expression.Parameter(typeof(IMapper), "mapper");
+
+            NewExpression newExpression = Expression.New(constructor, arg1, arg2, arg3);
+            LambdaExpression lambda = Expression.Lambda(newExpression, false, arg1, arg2, arg3);
+            Delegate result = lambda.Compile();
+
+            return (Func<DbContext, Type, IMapper, dynamic>)result;
+        }
+    }
+}

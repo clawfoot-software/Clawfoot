d9b8e5139b20bc6a1c0fbd162d25848cf22faa7a
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-07-16 18:45:41 -0700

    Adds CrudService Service Configuration

---
+++ Clawfoot.CrudService/ServiceConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Clawfoot.Core.Enums;
+using Clawfoot.Core.Interfaces;
+using Clawfoot.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.CrudService
+{
+    public static class ServiceConfiguration
+    {
+        /// <summary>
+        /// Registers the <see cref="ICrudService"/> with the DI container
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDefaultCrudService(this IServiceCollection services)
+        {
+            services.EnsureServiceRegistered(Utilities.ServiceConfiguration.ServicesCollection[ServiceTypes.DefaultAutoMapperProvider]);
+            services.EnsureServiceRegistered(Utilities.ServiceConfiguration.ServicesCollection[ServiceTypes.ForeignKeyPropertyCache]);
+
+            services.TryAddTransient<ICrudService, CrudService>();
+            return services;
+        }
+
+    }
+}

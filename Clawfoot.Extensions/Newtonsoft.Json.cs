5e42e448159002467e53eab1ecc83d006d2088dc
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-07-23 11:41:43 -0700

    Adds Newtonsoft.Json extensions

---
+++ Clawfoot.Extensions/Newtonsoft.Json.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Extensions.Newtonsoft.Json
+{
+    public static class Newtonsoft
+    {
+        public static bool IsJObjectStringEmpty(this JObject jObject, string key)
+        {
+            if (jObject[key] == null)
+            {
+                return true;
+            }
+            string value = (string)jObject[key];
+            return String.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool IsJTokenStringEmpty(this JToken jToken, string key)
+        {
+            if (jToken[key] == null)
+            {
+                return true;
+            }
+            string value = (string)jToken[key];
+            return String.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool IsJTokenBoolEmpty(this JToken jToken, string key)
+        {
+            if (jToken[key] == null)
+            {
+                return true;
+            }
+            try
+            {
+                bool value = (bool)jToken[key];
+                return false;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public static bool IsJObjectDictionaryEmpty(this JObject jObject, string key)
+        {
+            if (jObject[key] == null)
+            {
+                return true;
+            }
+            JObject value = (JObject)jObject[key];
+            return value.Count == 0;
+        }
+    }
+}

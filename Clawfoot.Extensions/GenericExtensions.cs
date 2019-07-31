d416eff2c2570e5bd018e3dc171e4722939c78f1
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-07-30 17:35:21 -0700

    Adds genericExtensions with new GetMemberName extension method

---
+++ Clawfoot.Extensions/GenericExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Extensions
+{
+    public static class GenericExtensions
+    {
+        //https://stackoverflow.com/a/56116478
+        /// <summary>
+        /// Gets the name of the method of type T
+        /// Usaed like myInstance.GetMemberName(x => nameof(x.MyMember))
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance"></param>
+        /// <param name="nameofMethod"></param>
+        /// <returns></returns>
+        public static string GetMemberName<T>(this T instance, Func<T, string> nameofMethod) where T : class
+        {
+            return nameofMethod(instance);
+        }
+    }
+}

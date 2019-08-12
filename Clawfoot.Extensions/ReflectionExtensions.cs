bc059728664a0f248ebf22019c48853fb059d7a4
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-08-12 13:51:08 -0700

    Adds reflection extension to exclude a property from a collection of properties by an attribute

---
+++ Clawfoot.Extensions/ReflectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Clawfoot.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Filters this collection by an attribute
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="attribute">The attribute to filter by</param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> ExcludeAttribute(this IEnumerable<PropertyInfo> items, Type attribute)
+        {
+            return items.Where(x => x.GetCustomAttribute(attribute) is null).ToList();
+        }
+    }
+}

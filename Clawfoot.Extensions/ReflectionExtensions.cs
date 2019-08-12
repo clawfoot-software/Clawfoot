aa3af4ad14303d1cd061b6af9d6cd8df911c9935
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-08-12 13:54:06 -0700

    Adds filter to only filter a collection of properties, exclusively, by an attribute

--- Clawfoot.Extensions/ReflectionExtensions.cs
+++ Clawfoot.Extensions/ReflectionExtensions.cs
@@ -9,7 +9,7 @@ namespace Clawfoot.Extensions
     public static class ReflectionExtensions
     {
         /// <summary>
-        /// Filters this collection by an attribute
+        /// Excludes all properties from the collection with the provided attribute
         /// </summary>
         /// <param name="items"></param>
         /// <param name="attribute">The attribute to filter by</param>
@@ -17,6 +17,17 @@ namespace Clawfoot.Extensions
         public static IEnumerable<PropertyInfo> ExcludeAttribute(this IEnumerable<PropertyInfo> items, Type attribute)
         {
             return items.Where(x => x.GetCustomAttribute(attribute) is null).ToList();
+        }
+
+        /// <summary>
+        /// Only includes properties from the collection with the provided attribute
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="attribute">The attribute to filter by</param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> IncludeOnlyAttribute(this IEnumerable<PropertyInfo> items, Type attribute)
+        {
+            return items.Where(x => !(x.GetCustomAttribute(attribute) is null)).ToList();
         }
     }
 }

d972cee2eae4fd547946d13d09913d5012bef6d6
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-08-12 13:55:02 -0700

    Adds XUnitSerializeable class

---
+++ Clawfoot.TestUtilities/XunitSerializable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Clawfoot.TestUtilities
+{
+    public class XUnitSerializable : IXunitSerializable
+    {
+        public void Deserialize(IXunitSerializationInfo info)
+        {
+            Type thisType = this.GetType();
+            PropertyInfo[] props = thisType.GetProperties();
+
+            foreach (PropertyInfo prop in props)
+            {
+                Type propType = prop.PropertyType;
+                if (propType.IsPrimitive)
+                {
+                    prop.SetValue(this, info.GetValue(prop.Name, propType));
+                }
+                else if (propType.IsAssignableFrom(typeof(IEnumerable<string>)))
+                {
+                    prop.SetValue(this, new HashSet<string>(info.GetValue<string[]>(prop.Name)));
+                }
+                else
+                {
+                    //throw new NotSupportedException($"Property Type {propType.Name} is not supported.");
+                }
+            }
+        }
+
+        public void Serialize(IXunitSerializationInfo info)
+        {
+            Type thisType = this.GetType();
+            PropertyInfo[] props = thisType.GetProperties();
+
+            foreach (PropertyInfo prop in props)
+            {
+                Type propType = prop.PropertyType;
+                if (propType.IsPrimitive || propType.IsEquivalentTo(typeof(string)))
+                {
+                    info.AddValue(prop.Name, prop.GetValue(this));
+                }
+                else if (typeof(IEnumerable<string>).IsAssignableFrom(propType))
+                {
+                    string[] testValue = ((IEnumerable<string>)prop.GetValue(this)).ToArray();
+                    info.AddValue(prop.Name, ((IEnumerable<string>)prop.GetValue(this)).ToArray());
+                }
+                else
+                {
+                    throw new NotSupportedException($"Property Type {propType.Name} is not supported.");
+                }
+            }
+        }
+    }
+}

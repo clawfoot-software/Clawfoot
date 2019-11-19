a65a9c2c06c8d0d6bc5aea58c0535f67c7e8bff8
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-11-18 16:07:59 -0800

    Adds basic Console project for misc testing

---
+++ TestingProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using Clawfoot.Status;
+using Clawfoot.Status.Interfaces;
+using System;
+
+namespace TestingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStatus();
+
+            Console.ReadLine();
+        }
+
+        static void TestStatus()
+        {
+            IStatus status = new Status();
+
+            status.AddError(Error.From(AuthError.RegistrationError,"some technical jargon", "An unexpected error has occurred"));
+            status.AddError(Error.From(AuthError.UserAlreadyExists, new []{ "gg@gg.com" }));
+            status.AddError(Error.From(AuthError.InvalidCredentials));
+
+            Console.WriteLine(status.ToString());
+            Console.WriteLine();
+            Console.WriteLine(status.ToUserFriendlyString());
+        }
+    }
+
+    public enum AuthError
+    {
+        [Error(Code = (int)InvalidCredentials, GroupName = "Login", Message = "Invalid Credentials")]
+        InvalidCredentials = 101,
+
+        [Error(Code = (int)RegistrationError, GroupName = "Registration")]
+        RegistrationError = 200,
+        [Error(Code = (int)UserAlreadyExists, GroupName = "Registration", Message = "User with this email address {0} already exists")]
+        UserAlreadyExists = 201,
+    }
+}
